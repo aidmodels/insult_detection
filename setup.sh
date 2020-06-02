64b16345569a892b352140fc22bd544ebfe44103
cyy <sheeppooo.cy@gmail.com>
2020-06-03 01:16:42 +0200

    modify requirement and setup unzip path

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-tar -zxvf ./pretrained/insults_data.tar.gz -C ./pretrained
-tar -zxvf ./pretrained/conversational_cased_L-12_H-768_A-12.tar.gz -C ./pretrained
-tar -zxvf ./pretrained/insults_kaggle_v4.tar.gz -C ./pretrained/
+tar -zxvf /app/pretrained/insults_data.tar.gz -C /app/pretrained
+tar -zxvf /app/pretrained/conversational_cased_L-12_H-768_A-12.tar.gz -C /app/pretrained
+tar -zxvf /app/pretrained/insults_kaggle_v4.tar.gz -C /app/pretrained/
